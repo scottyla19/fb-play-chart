a1c1c6ef496aa0e2a29170184e41135691e87c0e
Scott <scottyla19@gmail.com>
2016-09-30 06:21:01 -0600

    fixed ui added down and distance autofill

--- app.R
+++ app.R
@@ -1,4 +1,6 @@
 library(shiny)
+library(rhandsontable)
+
 source("helper.R")
 
 #df <- read.csv("fb_chart.csv")
@@ -9,7 +11,7 @@ df <- data.frame(offTeam = character(),
                  time = character(),
                  down=integer(),
                  distance=integer(),
-                 fieldPosition=character(),
+                 fieldPosition=integer(),
                  spot=character(),
                  personnel=character(),
                  playType=character(),
@@ -20,12 +22,11 @@ df <- data.frame(offTeam = character(),
                  passDirection=character(),
                  passDepth=integer(),
                  target=character(),
-                 puntLength=integer(),
-                 fgLength=integer(),
+                 
                  dFront = character(),
                  dBlitz = character(),
                  dCover = character(),
-                 outcome=character(),
+                 yardage=integer(),
                  note=character(),
                  stringsAsFactors=FALSE)
 
@@ -46,19 +47,18 @@ shinyApp(
           
           fluidRow(h3("Pre Snap"),
                    column(1, selectInput("offTeam", "Offense",choices = NULL)),
-                   #column(1,numericInput("drive","Series", 1 ,min = 1)),
-                   #column(1,numericInput("playNum","Play", 1 ,min = 1)),
                    column(1,numericInput("quarter","Quarter", 1 ,min = 1)),
                    column(1,textInput("time","Time")),
                    column(1,numericInput("down","Down", 1 ,min = 1, max = 4)),
                    column(1,numericInput("distance","Distance", 10,  min = 1, max = 99)),
-                   column(2,textInput("fieldPos","Field Position", placeholder = "OWN45 or OPP20") ),
+                   column(2,tags$div(title = "On your own side of field, dist = 100 - LOS otherwise dist = LOS",
+                                     textInput("fieldPos","Distance From GL")) ),
                    column(1, selectInput("spot", "Spot", list("LH", "Mid", "RH"))),
                    column(1,textInput("personnel","Personnel")),
                    column(1,textInput("dFront","Def. Front"))
                    ),
           fluidRow( h3("Post Snap"),
-                   column(1, selectInput("playType", "Play Type",list("Run","Pass","Punt", "FG"),selected = "Pass")),
+                   column(1, selectInput("playType", "Play Type",list("Run","Pass","Punt","FG"),selected = "Pass")),
                    conditionalPanel(
                      condition = "input.playType == 'Run'",
                         column(1,selectInput("runDirection","Direction",list("Right", "Left"))),
@@ -75,80 +75,91 @@ shinyApp(
                      column(1,textInput("dCover","Def. Cover", placeholder = "Cover 0, Cover 1, etc."))
                    ),
                    
-                   conditionalPanel(
-                     condition = "input.playType == 'Punt'",
-                     column(2,numericInput("puntLength", "Punt Length", 0))),
-                   
-                   conditionalPanel(
-                     condition = "input.playType == 'FG'",
-                     column(1,numericInput("fgLength", "FG Length", 15))),
-                   
                      column(2,textInput("dBlitz","Def. Blitz", placeholder = "Blitzers separated by comma")),
-                     column(1,textInput("outcome", "Outcome")),
+                     column(1,textInput("yardage", "Yardage")),
                      column(2, textInput("note", "Notes"))
           
                    
                    
                    ),
-          fluidRow(column(6,actionButton("submit","Submit Play")),
+          fluidRow(column(1),
+            
+                   column(4,actionButton("submit","Submit Play")),
                    
-                   column(6, downloadButton('downloadData')))
+                   column(4, downloadButton('downloadData')))
           ),
-          fluidRow(column(12, dataTableOutput('tbl')))
+          fluidRow(column(12, tabsetPanel(
+                                tabPanel("Data",rHandsontableOutput('tbl')),
+                                tabPanel("Summary", "Put summary stats here"),
+                                tabPanel("Heat Maps", "Put run/pass maps here")
+                              )
+                          )
+                   )
           )
           ,
   server = function(input, output, session) {
+    
     values <- reactiveValues(dfWorking = df)
     
+    #check for and allow users to update rows on the data table
+    observe({
+      if(!is.null(input$tbl))
+        values$dfWorking <- hot_to_r(input$tbl)
+      
+    })
     observeEvent(input$setTeams,{
       updateSelectInput(session, "offTeam",choices = list(input$homeTeam, input$awayTeam))
     })
     
     observeEvent(input$submit,{
+      validate(
+        need(input$down != "", "Please enter in a down from 1-4"),
+        need(input$distance != "", "Please enter in a distance"),
+        need(input$fieldPos != "", "Please enter in a field position"),
+        need(input$yardage != "", "Please enter in a yardage gained for the play.")
+        
+        
+        
+      )
+      #get values for updated down, distance, play, and drive counts
       curPlay = getCurrentPlay(values$dfWorking, input$offTeam)
       curDrive = getCurrentDrive(values$dfWorking, input$offTeam)
+      downAndDist <- getDownAndDistance(input$down,input$distance, input$yardage, input$fieldPos )
+      curDist = downAndDist[["dist"]]
+      curDown = downAndDist[["down"]]
+      curFieldPos = downAndDist[["fieldPos"]]
       
+      #get data from form
       newLine <- c(input$offTeam, curDrive, curPlay , input$quarter,
                    input$time, input$down, input$distance,
                    input$fieldPos, input$spot, input$personnel,
                    input$playType, input$runDirection,input$runGap,
                    input$play, input$rusher,input$passDirection,
-                   input$passDepth,input$target, input$puntLength,
-                   input$fgLength, input$dFront, input$dBlitz, input$dCover,input$outcome, input$note)
+                   input$passDepth,input$target,
+                   input$dFront, input$dBlitz, input$dCover,input$yardage, input$note)
       
-      
+      #add a new line to working dataframe
       values$dfWorking[nrow(values$dfWorking)+1,] <- newLine
       values$dfWorking <- values$dfWorking[order(as.numeric(rownames(values$dfWorking)),decreasing = TRUE),]
       
-     
-      
-      
       #update and reset inputs
       updateTextInput(session, "playNum", value = curPlay + 1)
+      updateTextInput(session, "distance", value = curDist)
+      updateTextInput(session, "down", value = curDown)
+      updateTextInput(session, "fieldPos", value = curFieldPos)
       updateTextInput(session, "target", value = "")
       updateTextInput(session, "personnel", value = "")
       updateTextInput(session, "play", value = "")
-      updateTextInput(session, "outcome", value = "")
+      updateTextInput(session, "yardage", value = "")
       updateTextInput(session, "rusher", value = "")
       updateNumericInput(session, "passDepth", value = 0)
       
+  #render hands on table with updated values
+      output$tbl <- renderRHandsontable({
+        rhandsontable(values$dfWorking)
+      })
       
-      
-      
-     
-      #series	playNum	quarter	time	down	distance	fieldPosition	spot	personnel	playType	runDirection	
-      #runGap	runPlay	rusher	passDirection	passDepth	target	puntLength	fgLength	outcome
-     
-      
-      
-      output$tbl = renderDataTable(
-        values$dfWorking,
-        options = list(
-          
-        )
-        
-      )
-      
+      #download as csv
       output$downloadData <- downloadHandler(
         filename = function() { paste(date(), '.csv', sep='') },
         content = function(file) {
--- helper.R
+++ helper.R
@@ -1,3 +1,42 @@
+isFirstDown <- FALSE
+
+getDown <- function(down){
+  down <- as.integer(down)
+  if(isFirstDown){
+    return (1)
+  }else{
+    if(down > 3){
+      return(1)
+    }else{
+      return(down + 1)
+    }
+    
+  }
+}
+
+getDownAndDistance <- function(down,dist, gain, fieldPos){
+  gain <- as.integer(gain)
+  dist <- as.integer(dist)
+  down <- as.integer(down)
+  fieldPos <- as.integer(fieldPos)
+  
+  if (gain >= dist){
+    newDown = 1
+    newDist = 10
+    
+  }else{
+    if(down > 3){
+      newDown = 1
+    }else{
+      newDown <- down + 1
+    }
+    newDist <- dist - gain
+    
+  }
+  newFieldPos = fieldPos - gain
+  return(list(down = newDown, dist = newDist, fieldPos =newFieldPos ))
+}
+
 getCurrentPlay <- function(df,team){
   df2 <- df[df$offTeam == team,]
   curPlay <- nrow(df2) + 1
@@ -10,7 +49,6 @@ getCurrentDrive <- function(df,team){
   curDrive = 1
   if (nrow(df) >0){
     if (df$offTeam[1] != team){
-      print("drive change")
       if (team %in% df$offTeam){
         df2 <- df[df$offTeam == team,]
         curDrive = as.numeric(df2$drive[1])+1
@@ -19,7 +57,6 @@ getCurrentDrive <- function(df,team){
       }
       
     }else{
-      print("same drive")
       df2 <- df[df$offTeam == team,]
       curDrive = as.numeric(df2$drive[1])
     }
